58f15dcc044e616d9faa986954aee28c24d8b7dd
ashwin2001-cloud <57358373+ashwin2001-cloud@users.noreply.github.com>
2021-06-05 21:53:55 +0530

    Delete routes directory

--- routes/resume.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-const router=express.Router();
-const resumeController=require('../controllers/resume_controller');
-
-
-router.post('/create',resumeController.create);
-router.get('/',resumeController.resumePage);
-console.log('resume router is working');
-module.exports=router;
